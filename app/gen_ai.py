ff3f5aa6ae3668fb2799e8948e02becf23b3e8b6
iKronyx <ikronyx@gmail.com>
2025-08-04 09:07:39 +0530

    gemini added 1

---
+++ app/gen_ai.py
@@ -0,0 +1,11 @@
+import google.generativeai as genai
+import os
+
+api_key_value = open("api_key.txt", 'r', encoding='utf-8').read().strip()
+
+genai.configure(api_key=api_key_value)
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+def generate_description(collective_input):
+    response = model.generate_content("\'" + collective_input + "\' " + "Tell me a story of a day in the life after I achieve these")
+    return response.text
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from text_utils import split_text_into_5_parts, summarize_chunk, split_paragraph_by_period
 from image_utils import get_pollinations_url
+from gen_ai import generate_description
 
 st.set_page_config(page_title="Text-to-Image Summarizer", layout="wide")
 
@@ -9,13 +10,24 @@ st.markdown("Paste a long text description below. We'll split, summarize, and ge
 
 user_input = st.text_area("Your Input Text", height=300)
 
-if user_input:
-    with st.spinner("Processing..."):
-        parts = split_paragraph_by_period(user_input)
-        # summaries = [summarize_chunk(p) for p in parts]
+future_goals = st.text_input("What are future goals?")
+desires = st.text_input("What are your desires?")
 
-        st.header("🖼️ Results")
-        for i, summary in enumerate(parts):
-            image_url = get_pollinations_url(summary)
-            st.subheader(f"Part {i+1}")
-            st.image(image_url, caption=summary)
+future_goals = "My future goals are: " + future_goals
+desires = "My desires are: " + desires
+
+collective_input = future_goals + desires
+
+if future_goals and desires:
+    st.text(collective_input)
+
+# if user_input:
+#     with st.spinner("Processing..."):
+#         parts = split_paragraph_by_period(user_input)
+#         # summaries = [summarize_chunk(p) for p in parts]
+
+#         st.header("🖼️ Results")
+#         for i, summary in enumerate(parts):
+#             image_url = get_pollinations_url(summary)
+#             st.subheader(f"Part {i+1}")
+#             st.image(image_url, caption=summary)
