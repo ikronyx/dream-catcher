77f4c537ffb75dcc54c4ec3fe7283cfbf60be206
iKronyx <ikronyx@gmail.com>
2025-08-05 04:47:30 +0530

    up main

--- app/main.py
+++ app/main.py
@@ -37,5 +37,23 @@ st.button('Take Me There', on_click=click_button, use_container_width=True, type
 
 if st.session_state.clicked:
     if future_goals and desires:
-        story = generate_description(collective_input)
-        st.text(story)
+        # story = generate_description(collective_input)
+        # st.text(story)
+
+
+        @st.dialog("Cast your vote")
+        def vote(item):
+            st.write(f"Why is {item} your favorite?")
+            reason = st.text_input("Because...")
+            if st.button("Submit"):
+                st.session_state.vote = {"item": item, "reason": reason}
+                st.rerun()
+
+        if "vote" not in st.session_state:
+            st.write("Vote for your favorite")
+            if st.button("A"):
+                vote("A")
+            if st.button("B"):
+                vote("B")
+        else:
+            f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
