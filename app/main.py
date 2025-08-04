5e800467911e5e2e5246afedd543c24b0e114214
iKronyx <ikronyx@gmail.com>
2025-08-05 04:50:59 +0530

    up main

--- app/main.py
+++ app/main.py
@@ -40,20 +40,17 @@ if st.session_state.clicked:
         # story = generate_description(collective_input)
         # st.text(story)
 
-
         @st.dialog("Cast your vote")
         def vote(item):
-            st.write(f"Why is {item} your favorite?")
-            reason = st.text_input("Because...")
-            if st.button("Submit"):
-                st.session_state.vote = {"item": item, "reason": reason}
-                st.rerun()
+            story = generate_description(collective_input)
+            st.write(f"A day in your life!")
+            st.text(story)
 
-        if "vote" not in st.session_state:
-            st.write("Vote for your favorite")
-            if st.button("A"):
-                vote("A")
-            if st.button("B"):
-                vote("B")
-        else:
-            f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
+        # if "vote" not in st.session_state:
+        #     st.write("Vote for your favorite")
+        #     if st.button("A"):
+        #         vote("A")
+        #     if st.button("B"):
+        #         vote("B")
+        # else:
+        #     f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
